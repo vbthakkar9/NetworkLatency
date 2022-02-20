b2b7378445b8cd53c8d5f2f300271b3ec512dfa3
Viral Thakkar <viralthakkar86@gmail.com>
2022-02-20 16:15:44 +0530

    Update Final-Analysis.sh

--- Final-Analysis.sh
+++ Final-Analysis.sh
@@ -10,10 +10,6 @@ tshark -r test.pcap -q -z io,stat,0,"COUNT(tcp.analysis.retransmission) tcp.anal
 echo ""
 tshark -r test.pcap -q -z io,stat,5,"COUNT(tcp.analysis.retransmission) tcp.analysis.retransmission"
 echo ""
-rm -rf temp.txt
-tshark -r test.pcap -2Y "tcp.analysis.ack_rtt" -T fields -e tcp.analysis.ack_rtt | sort -rn >> temp.txt
-head -`echo scale=0\;$(cat temp.txt|wc -l)\*5/100 | bc -l` temp.txt |awk '{s+=$1}END{print "Latency p95 :",s/NR}' RS="\n"
-head -`echo scale=0\;$(cat temp.txt|wc -l)\*1/100 | bc -l` temp.txt |awk '{s+=$1}END{print "Latency p99 :",s/NR}' RS="\n"
-head -`echo scale=0\;$(cat temp.txt|wc -l)\*100/100 | bc -l` temp.txt |awk '{s+=$1}END{print "Latency Average :",s/NR}' RS="\n"
+java -jar LatencyReport.jar
 
 
