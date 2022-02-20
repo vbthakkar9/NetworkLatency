d2ba9dfae1047958e81bdaea49b8fbc432187609
Viral Thakkar <viralthakkar86@gmail.com>
2022-02-20 16:16:41 +0530

    Update Final-Analysis.sh

--- Final-Analysis.sh
+++ Final-Analysis.sh
@@ -6,9 +6,9 @@ rm -rf test*
 echo "Final pcap file generarted"
 cd /home/java
 echo "Analysis pcap file"
-tshark -r test.pcap -q -z io,stat,0,"COUNT(tcp.analysis.retransmission) tcp.analysis.retransmission"
+tshark -r test.pcap -q -z io,stat,10,"COUNT(tcp.analysis.retransmission) tcp.analysis.retransmission"| grep -P "\d+\.?\d*\s+<>\s+|Interval +\|" | tr "|" " " | sed -E 's/<>/-/;'
 echo ""
-tshark -r test.pcap -q -z io,stat,5,"COUNT(tcp.analysis.retransmission) tcp.analysis.retransmission"
+tshark -n -r test.pcap -Y "tcp.analysis.retransmission" -T fields -e tcp.stream | wc -l
 echo ""
 java -jar LatencyReport.jar
 
